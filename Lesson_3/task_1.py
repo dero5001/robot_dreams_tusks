c035b02f24a33fe435d3b5a9cf975e2f1f3dbbca
dero5001 <denysyuk.roman92@gmail.com>
2022-12-11 22:12:34 +0200

    Tusk_1

--- Lesson_3/task_1.py
+++ Lesson_3/task_1.py
@@ -1,4 +1,4 @@
 i = 0
 while i < 42:
-  print('I love Python')
-  i += 1
+    print('I love Python')
+    i += 1
--- Lesson_3/tusk_2-4.py
+++ Lesson_3/tusk_2-4.py
@@ -1,5 +1,5 @@
 age_in_month = 365
 age_in_years = age_in_month / 12
-#print(age_in_years)    #test
+# print(age_in_years)    #test
 my_age = f'Му name is Roman I’m {int(age_in_years)} years old'
-#print(my_age)      #test
+# print(my_age)      #test

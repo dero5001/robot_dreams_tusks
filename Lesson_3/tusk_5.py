fe18887234c056bf73e87329198debb564e8b44e
dero5001 <denysyuk.roman92@gmail.com>
2022-12-13 20:33:29 +0200

    Fix for Tusk_5

--- Lesson_3/tusk_5.py
+++ Lesson_3/tusk_5.py
@@ -1,21 +1,6 @@
 i = 1
-if i < 5:
-    print('i is less than 5')
-else:
-    print('i is more than 5')
-if i > 5:
-    print('i is more than 5')
-else:
-    print('i is less than 5')
-if i == 5:
-    print('i equals 5')
-else:
-    print('i is not equal to 5')
-if i != 5:
-    print('i is not equal to 5')
-else:
-    print('i equals 5')
-if i >= 5:
-    print('i is more than 5 or equal')
-else:
-    print('i is less than 5')
+print(i < 5)
+print(i > 5)
+print(i == 5)
+print(i != 5)
+print(i >= 5)

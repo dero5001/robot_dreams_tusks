a521ccdbcc6a896e935e0c57f4c5da0bd92f1760
dero5001 <denysyuk.roman92@gmail.com>
2022-12-14 16:58:04 +0200

    Tusk for lesson 4 is done

---
+++ Lesson_4/Tusk_1-3.py
@@ -0,0 +1,10 @@
+text = input('please input something: ')
+if text.isdigit():
+  if int(text) == 0:
+    print ('You entered a number and it is zero')
+  elif int(text) % 2 == 0:
+    print ('You entered a number and it is even')
+  else:
+    print ('You entered a number and it is odd')
+else:
+  print (f'You entered a text with {len(text)} symbols')

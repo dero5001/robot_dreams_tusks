4f4b44a71337c48af14724868280901c6e4d23d7
dero5001 <denysyuk.roman92@gmail.com>
2023-03-29 09:06:30 +0300

    Lesson 30 fix

--- Lesson_30/Tusk_2.py
+++ Lesson_30/Tusk_2.py
@@ -5,15 +5,23 @@ city_name = input('Введіть назву міста: ')
 geo_url = f'https://geocoding-api.open-meteo.com/v1/search?name={city_name}'
 
 res_geo = requests.get(geo_url).json()
-data = res_geo["results"]
-longitude = data[0]['longitude']
-latitude = data[0]['latitude']
 
-url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true"
-res = requests.get(url).json()
-curren_weather = res['current_weather']
+if "results" in res_geo:
+    data = res_geo["results"]
+    longitude = data[0]['longitude']
+    latitude = data[0]['latitude']
 
-print(f'Поточна погода у місті {city_name}:')
-print(f'Температура: {curren_weather["temperature"]}°C')
-print(f'Швидкість вітру: {curren_weather["windspeed"]} м/с')
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true"
+    res = requests.get(url).json()
+    print(res)
+    curren_weather = res['current_weather']
+
+    print(f'Поточна погода у місті {city_name}:')
+    print(f'Температура: {curren_weather["temperature"]}°C')
+    print(f'Швидкість вітру: {curren_weather["windspeed"]} м/с')
+
+else:
+    print('Таке місто не знайдено')
+
+
 

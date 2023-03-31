13b3277a87cab9b45472df82420fb3b3c6507e6e
dero5001 <denysyuk.roman92@gmail.com>
2023-03-31 11:12:24 +0300

    Lesson 31 fix

--- flask-homework/main.py
+++ flask-homework/main.py
@@ -1,11 +1,22 @@
+from logging.config import dictConfig
 from flask import Flask
+
+dictConfig({
+    "version": 1,
+    "formatters": {
+        "simple": {"format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"}
+    },
+    "handlers": {
+        "console": {"class": "logging.StreamHandler", "formatter": "simple"}
+    },
+    "root": {"handlers": ["console"], "level": "INFO"},
+})
 
 app = Flask(__name__)
 
-
 @app.route('/hello')
 def hello():
-    app.logger.info(print('Hello func was called'))
+    app.logger.info(f'Hello func was called')
     return 'Hello World!'
 
 

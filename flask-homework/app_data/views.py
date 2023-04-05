e855bb557140257f238ab3496cfdbfc7f895175c
dero5001 <denysyuk.roman92@gmail.com>
2023-04-05 22:41:45 +0300

    Lesson 34

--- flask-homework/app_data/views.py
+++ flask-homework/app_data/views.py
@@ -1,5 +1,9 @@
 from app_data import app
-from flask import request, redirect
+from flask import request, redirect, render_template, session
+import  random
+
+
+app.secret_key = b'robot'
 
 
 @app.route('/')
@@ -15,72 +19,84 @@ def hello():
 
 @app.route('/users')
 def users():
-    return (
-        '<ul>'
-        '<li>Roman</li>'
-        '<li>Anton</li>'
-        '<li>Nazar</li>'
-        '<li>Dima</li>'
-        '<li>Andrii</li>'
-        '</ul>'
-    )
+    current_user = session.get('user')
+    if current_user:
+        users_list = ['Voldemort',
+                      'Sirius Black',
+                      'Remus Lupin',
+                      'Neville Longbottom',
+                      'Minerva McGonagall',
+                      'Albus Dumbledore',
+                      'Severus Snape']
+        requested_list = []
+        random_list = random.sample(users_list, random.randint(1, len(users_list)))
+        for user in random_list:
+            requested_list += (user,)
+        return render_template('list.html', name='Users', list=requested_list, username=current_user)
+    else:
+        return redirect('/login')
 
 
-@app.route('/users/<user_id>')
+@app.route('/users/<int:user_id>')
 def users_id_check(user_id):
-    if user_id.isdigit():
+    current_user = session.get('user')
+    if current_user:
         if int(user_id) % 2 == 0:
-            return f'Requested user ID: {user_id}'
+            return render_template('user_id.html', name=f'User: {user_id}', user_id=user_id, username=current_user)
         else:
             return '', 404
     else:
-        return 'Users ID have to be digit', 404
+        return redirect('/login')
 
 
 @app.route('/books')
 def books():
-    return ('<ul>'
-    "<li>Harry Potter and the Philosopher's Stone</li>"
-    '<li>Harry Potter and the Chamber of Secrets</li>'
-    '<li>Harry Potter and the Prisoner of Azkaban</li>'
-    '<li>Harry Potter and the Goblet of Fire</li>'
-    '<li>Harry Potter and the Order of the Phoenix</li>'
-    '<li>Harry Potter and the Half-Blood Prince</li>'
-    '<li>Harry Potter and the Deathly Hallows</li>'
-    '</ul>')
+    current_user = session.get('user')
+    if current_user:
+        books_list = [
+            "Harry Potter and the Philosopher's Stone",
+            'Harry Potter and the Chamber of Secrets',
+            'Harry Potter and the Prisoner of Azkaban',
+            'Harry Potter and the Goblet of Fire',
+            'Harry Potter and the Order of the Phoenix',
+            'Harry Potter and the Half-Blood Prince',
+            'Harry Potter and the Deathly Hallows'
+        ]
+        requested_list = []
+        random_list = random.sample(books_list, random.randint(1, len(books_list)))
+        for book in random_list:
+            requested_list += (book,)
+        return render_template('list.html', name='Books', list=requested_list, username=current_user)
+    else:
+        return redirect('/login')
 
 
 @app.route('/books/<books_title>')
 def books_title_check(books_title):
-    return f'Requested book: {books_title.capitalize()}'
+    current_user = session.get('user')
+    if current_user:
+        return render_template('new_book.html', name=f'{books_title}', book_name=books_title, username=current_user)
+    else:
+        return redirect('/login')
 
 
 @app.route('/params')
 def params_page():
-    _args = (
-            '<style>table, th, td {border: 1px solid black;border-collapse: collapse;}'
-            '</style><table><tr><th>Parameter</th><th>Value</th></tr>'
-    )
-    for key, value in request.args.items():
-        _args += f'<tr><th>{key}</th><th>{value}</th></tr>'
-    _args += '</table>'
-    print(_args)
-    return f'{_args}'
+    current_user = session.get('user')
+    if current_user:
+        return render_template('params.html', list=request.args.items(), username=current_user)
+    else:
+        return redirect('/login')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
     if request.method == 'GET':
-        html_form = (
-            "<form method=POST action='/login'>"
-            "<input type='string' name='username' value=''/>"
-            "<input type='password' name='password' value=''/>"
-            "<button type='submit'>Login</button>"
-            "</form>"
-        )
-        return html_form
+        return render_template('login.html')
     elif request.method == 'POST':
         if 'password' and 'username' in request.form:
+            username = request.form.get('username')
+            session['user'] = username
             return redirect('/users')
-        else:
-            return 'Password incorrect', 404
+    else:
+        return 'Password incorrect', 404

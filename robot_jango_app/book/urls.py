e52a4bb43d2fae599f8752976266d3e9cdc7bf81
dero5001 <denysyuk.roman92@gmail.com>
2023-05-08 19:53:24 +0300

    Django_3_41

--- robot_jango_app/book/urls.py
+++ robot_jango_app/book/urls.py
@@ -4,6 +4,6 @@ from .views import BooksList, DetailBook, CreateBook
 
 urlpatterns = [
     path('', BooksList.as_view(), name='Books list'),
-    path('create', CreateBook, name='Create Book'),
+    path('create', CreateBook.as_view(), name='Create Book'),
     path('<int:pk>', DetailBook.as_view(), name='Detail Book')
 ]

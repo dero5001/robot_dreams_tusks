23d4fbd6aa1ffbc84c08e365b8522e5653503c37
dero5001 <denysyuk.roman92@gmail.com>
2023-05-17 22:33:13 +0300

    Django_3_467

--- robot_jango_app/robot/settings.py
+++ robot_jango_app/robot/settings.py
@@ -98,9 +98,13 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "robot",
+        "USER": "robot",
+        "PASSWORD": "robot",
+        "HOST": "0.0.0.0",
+        "PORT": "5432",
     }
 }
 
